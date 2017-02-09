2386afe249fe3c543896a7f0a3fe561e42be1b23
袁小荣 <664995775@qq.com>
2017-02-09 10:37:05 +0800

    项目初始化,第一次提交

---
+++ BaseSetting/BaseSetting/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseSetting
+//
+//  Created by 袁小荣 on 2017/2/9.
+//  Copyright © 2017年 袁小荣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
