dc7687725d124e03545159ae0704a7fdc56d7c87
袁小荣 <664995775@qq.com>
2017-02-09 11:33:40 +0800

    添加头文件

---
+++ XYBaseSettingExample/XYBaseSetting/XYBaseSetting.h
@@ -0,0 +1,13 @@
+//
+//  XYBaseSetting.h
+//  XYBaseSettingExample
+//
+//  Created by 袁小荣 on 2017/2/9.
+//  Copyright © 2017年 袁小荣. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XYBaseSetting : NSObject
+
+@end
