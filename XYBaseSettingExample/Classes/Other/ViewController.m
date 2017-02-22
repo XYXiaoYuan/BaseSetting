664b989ea3d33b0bb3b6f3329cdedc3e3130b233
袁小荣 <664995775@qq.com>
2017-02-22 16:34:16 +0800

    C语言的static函数处理

--- XYBaseSettingExample/Classes/Other/ViewController.m
+++ XYBaseSettingExample/Classes/Other/ViewController.m
@@ -8,17 +8,67 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
 
-@end
+
+static  NSDateFormatter *XMLYDataCreateFormatter(NSString *string) {
+    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
+    formatter.locale = [[NSLocale alloc] initWithLocaleIdentifier:@"en_US_POSIX"];
+    formatter.dateFormat = string;
+    return formatter;
+}
+
+
+static  NSDateFormatter *XMLYDateFormatter(NSString *string) {
+    if(!string || ![string isKindOfClass:[NSString class]] || string.length == 0) return nil;
+    static CFMutableDictionaryRef cache;
+    static dispatch_semaphore_t lock;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        cache = CFDictionaryCreateMutable(CFAllocatorGetDefault(), 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+        lock = dispatch_semaphore_create(1);
+    });
+    
+    dispatch_semaphore_wait(lock, DISPATCH_TIME_FOREVER);
+    NSDateFormatter *formatter = CFDictionaryGetValue(cache, (__bridge const void *)(string));
+    dispatch_semaphore_signal(lock);
+    
+    if(!formatter) {
+        formatter = XMLYDataCreateFormatter(string);
+        if(formatter) {
+            dispatch_semaphore_wait(lock, DISPATCH_TIME_FOREVER);
+            CFDictionarySetValue(cache, (__bridge const void *)(string), (__bridge const void *)(formatter));
+            dispatch_semaphore_signal(lock);
+        }
+    }
+    return formatter;
+}
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSString *hello = [self getHelloStringByLocalTime];
+    
+    NSLog(@"%@",hello);
 }
 
+
+- (NSString *)getHelloStringByLocalTime {
+    NSDateFormatter *formatter = XMLYDateFormatter(@"HH:mm:ss");
+    NSString *dateString = [formatter stringFromDate:[NSDate date]];
+    NSArray *resultArr = [dateString componentsSeparatedByString:@":"];
+    if(resultArr.count == 0) return nil;
+    NSInteger hour = [resultArr.firstObject integerValue];
+    if(hour > 6 && hour < 11) {
+        return @"早安";
+    }else if(hour >= 11 && hour <= 4) {
+        return @"午安";
+    }else{
+        return @"晚安";
+    }
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
